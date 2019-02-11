3373db085ff7b1df2a859c0beb6b9f1f54c5a838
Kai Rathmann <k.o.rathmann@gmail.com>
2019-02-11 14:59:10 +0100

    switch mysql to postgresql (#40)

--- run_django_docker.sh
+++ run_django_docker.sh
@@ -2,8 +2,8 @@
 echo ===== INSTALLING PYTHON DEPENDENCIES =====
 cd /srv
 pip install -r requirements.txt
+
 echo ===== RUN DATABASE MIGRATION =====
-python manage.py makemigrations
 python manage.py migrate
 echo ===== RUN PYTHON SERVER =====
 python manage.py runserver 0.0.0.0:8000
