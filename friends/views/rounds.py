69d357cb9618de9494747cc2f5ac1f80a005ff13
Kai Rathmann <k.o.rathmann@gmail.com>
2019-02-22 14:19:01 +0100

    fix static rounds return (#73)

--- friends/views/rounds.py
+++ friends/views/rounds.py
@@ -7,12 +7,12 @@ class Rounds(APIView):
 
     def get(self, request):
         return Response(
-"""[
-    {
-        "id": 1,
-        "start_timestamp": "2019-01-01T00:00:00Z",
-        "end_timestamp": "2019-12-31T23:59:59.999999Z",
-        "description": "",
-        "is_subscribed": true
-    }
-]""", status=status.HTTP_200_OK)
+            [
+                {
+                    "id": 1,
+                    "start_timestamp": "2019-01-01T00:00:00Z",
+                    "end_timestamp": "2019-12-31T23:59:59.999999Z",
+                    "description": "",
+                    "is_subscribed": True,
+                }
+            ], status=status.HTTP_200_OK)
