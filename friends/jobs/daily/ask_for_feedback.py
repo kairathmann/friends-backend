010538567801776e2b30e287c439a2e76ac26e9c
Dariusz Górak <darekg11@tlen.pl>
2019-03-11 18:19:53 +0100

    Add Notification types to OneSignal notification payloads (#100)

--- friends/jobs/daily/ask_for_feedback.py
+++ friends/jobs/daily/ask_for_feedback.py
@@ -5,12 +5,15 @@ from django_extensions.management.jobs import BaseJob
 
 from friends import models
 from friends.utilities.push_notifications import NotificationService
+from friends.utilities.user_utils import UserUtils
 
 class Job(BaseJob):
     help = "Ask users for feedback."
     def __init__(self, ignore_calendar=False):
         super().__init__()
         self.ignore_calendar = ignore_calendar
+
+        self.brian_bot = UserUtils.get_brian_bot()
 
     def should_we_execute(self):
         if self.ignore_calendar:
@@ -36,11 +39,24 @@ class Job(BaseJob):
         return True
 
     def ask_for_feedback(self, chat):
-        for chatuser in chat.chatusers_set.all():
-            logging.log(logging.INFO, f"Asking user {chatuser.user_id} for feedback in chat {chatuser.chat_id}")
-            chatuser.feedback_requested = True
-            chatuser.save()
-            self.notify_participant(chat, chatuser.user)
+        chatusers = chat.chatusers_set.all()[::1]
+        chatuser_a = chatusers[0]
+        chatuser_b = chatusers[1]
+        logging.log(logging.INFO, f"Asking user {chatuser_a.user_id} and {chatuser_b.user_id} for feedback in chat {chat.id}")
 
-    def notify_participant(self, chat, user):
-        NotificationService().dispatch_feedback_request_notification(chat, user)
+        chatuser_a.feedback_requested = True
+        chatuser_a.save()
+        self.notify_participant(chat, chatuser_a.user, chatuser_b.user)
+
+        chatuser_b.feedback_requested = True
+        chatuser_b.save()
+        self.notify_participant(chat, chatuser_b.user, chatuser_a.user)
+
+    def notify_participant(self, chat, user, match_partner):
+        luminos_bot_message = models.Message.objects.create(
+            chat_id=chat.id,
+            text=f'How did it go with {match_partner.first_name}? You can now submit feedback on your experience with {match_partner.first_name} to help us improve in the future.',
+            sender=self.brian_bot,
+        )
+
+        NotificationService().dispatch_feedback_request_notification(luminos_bot_message, chat, user)
